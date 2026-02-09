0aa0d824a536a6af2a5bc867f28a56a013d503ca
Muhammad Azka Awliya <azkaawliya06@gmail.com>
2026-02-09 14:45:56 +0700

    punya gw

---
+++ kalkulatorMain.py
@@ -0,0 +1,18 @@
+class Calculator:
+    def add(self, a, b):
+        return a + b
+
+    def subtract(self, a, b):
+        return a - b
+
+    def multiply(self, a, b):
+        return a * b
+
+    def divide(self, c, d):
+        return c / d
+
+    def modulo(self, a, b):
+        return a % b
+
+    def power(self, a, b):
+        return a ** b
