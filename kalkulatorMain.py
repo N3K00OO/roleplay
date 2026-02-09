ec61a63a84e2c3d8d9ca94bec7ef8e847ef0526a
Muhammad Azka Awliya <azkaawliya06@gmail.com>
2026-02-09 14:46:36 +0700

    el dino

---
+++ kalkulatorMain.py
@@ -0,0 +1,18 @@
+class Calculator:
+    def add(self, a, b):
+        return a + b
+
+    def subtract(self, a, b):
+        return a - b
+
+    def multiply(self, x, y):
+        return x * y
+
+    def divide(self, a, b):
+        return a / b
+
+    def modulo(self, a, b):
+        return a % b
+
+    def power(self, a, b):
+        return a ** b
